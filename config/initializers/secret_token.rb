1e3da149d17492ccd57212e2d75bba947d1d7d46
Ted Haile <theodore.haile@gmail.com>
2013-05-09 17:55:20 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackSites::Application.config.secret_token = 'a3d7c9c72130cf700091128ff50b10d885b5cab0ee68fbb3e0192f6645d9692a370da24f27ead427fcf1130060e8e2b077d746b6b6bbced19020dbd1e58f826e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TrackSites::Application.config.session_store :cookie_store, key: '_track_sites_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TrackSites::Application.config.session_store :active_record_store
